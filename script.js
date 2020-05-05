6412727f3e19cdbc1ef6b2292aa1a89ece83fb42
I-am-aman <readyaman@gmail.com>
2020-05-05 14:29:21 +0530

    HTML5 Canvas added and done

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
 $(function(){
-    $("#slider").slider();
+
+    var paint = false;
+    var paint_erase = "paint";
+    var canvas = document.getElementById("paint");
+    var ctx = canvas.getContext("2d");
+    var container = $("#container");
+    var mouse = {x: 0, y: 0};
+
+    if(localStorage.getItem("imgCanvas") != null){
+        var img = new Image();
+        img.onload = function(){
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = localStorage.getItem("imgCanvas");
+    }
+
+    ctx.lineWidth = 3;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+
+    container.mousedown(function(event){
+        paint = true;
+
+        ctx.beginPath();
+        mouse.x = event.pageX - this.offsetLeft;
+        mouse.y = event.pageY - this.offsetTop;
+        ctx.moveTo(mouse.x, mouse.y);
+    });
+
+    container.mousemove(function(event){
+        mouse.x = event.pageX - this.offsetLeft;
+        mouse.y = event.pageY - this.offsetTop;
+
+        if(paint == true){
+            if(paint_erase == "paint"){
+                ctx.strokeStyle = $("#paintColor").val();
+            }else{
+                ctx.strokeStyle = "white";
+            }
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+    });
+
+    container.mouseup(function(){
+        paint = false;
+    });
+
+    container.mouseleave(function(){
+        paint = false;
+    });
+
+    $("#erase").click(function(){
+        if(paint_erase == "paint")
+            paint_erase = "erase";
+        else    
+            paint_erase = "paint";
+
+        $(this).toggleClass("eraseMode");
+    });
+
+    $("#save").click(function(){
+        if(typeof(localStorage) != null)
+            localStorage.setItem("imgCanvas", canvas.toDataURL());
+        else
+            window.alert("Your Browser doen not support local storage");
+    });
+
+    $("#reset").click(function(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        paint_erase = "paint";
+        $("#erase").removeClass("eraseMode");
+    });
+
+    $("#paintColor").change(function(){
+        $("#circle").css("background-color", $(this).val());
+    });
+
+    $("#slider").slider({
+        min: 3,
+        max: 30,
+        slide: function(event, ui){
+            $("#circle").height(ui.value);
+            $("#circle").width(ui.value);
+            ctx.lineWidth = ui.value;
+        }
+    });
+    
 });
